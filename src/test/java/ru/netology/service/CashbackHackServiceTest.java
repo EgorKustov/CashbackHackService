e9b9e83eefd6a76ef100acf52685d7504f141a9d
Egor Kustov <egorkustov@icloud.com>
2023-11-29 17:32:09 +0300

    add more tests

--- src/test/java/ru/netology/service/CashbackHackServiceTest.java
+++ src/test/java/ru/netology/service/CashbackHackServiceTest.java
@@ -1,46 +1,77 @@
 package ru.netology.service;
 
-import org.junit.Assert;
-import org.junit.Test;
+//import org.junit.Assert;
+//import org.junit.Test;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 public class CashbackHackServiceTest {
 
+//    @Test
+//    public void shouldReturnRemainIfAmountIsLessThanBoundary() {
+//
+//        CashbackHackService cashbackHackService = new CashbackHackService();
+//        int amount = 500;
+//
+//        int result = cashbackHackService.remain(amount);
+//
+//        Assert.assertEquals(500, result);
+//    }
+//
+//    @Test
+//    public void shouldReturnZeroIfAmountIsEqualToBoundary() {
+//
+//        CashbackHackService cashbackHackService = new CashbackHackService();
+//        int amount = 1000;
+//
+//        int result = cashbackHackService.remain(amount);
+//
+//        Assert.assertEquals(0, result);
+//    }
+//
+//    @Test
+//    public void shouldReturnRemainIfAmountIsGreaterThanBoundary() {
+//
+//        CashbackHackService cashbackHackService = new CashbackHackService();
+//        int amount = 1200;
+//
+//        int result = cashbackHackService.remain(amount);
+//
+//        Assert.assertEquals(800, result);
+//    }
+//}
+
     @Test
-    public void shouldReturnRemainIfAmountIsLessThanBoundary() {
-        // Arrange
+    public void shouldReturnRemainIfAmountIsLessThanBoundary1() {
+
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 500;
 
-        // Act
         int result = cashbackHackService.remain(amount);
 
-        // Assert
-        Assert.assertEquals(500, result);
+        assertEquals(500, result);
     }
 
     @Test
-    public void shouldReturnZeroIfAmountIsEqualToBoundary() {
-        // Arrange
+    public void shouldReturnZeroIfAmountIsEqualToBoundary2() {
+
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1000;
 
-        // Act
         int result = cashbackHackService.remain(amount);
 
-        // Assert
-        Assert.assertEquals(0, result);
+        assertEquals(0, result);
     }
 
     @Test
-    public void shouldReturnRemainIfAmountIsGreaterThanBoundary() {
-        // Arrange
+    public void shouldReturnRemainIfAmountIsGreaterThanBoundary3() {
+
         CashbackHackService cashbackHackService = new CashbackHackService();
         int amount = 1200;
 
-        // Act
         int result = cashbackHackService.remain(amount);
 
-        // Assert
-        Assert.assertEquals(800, result);
+        assertEquals(800, result);
     }
 }
